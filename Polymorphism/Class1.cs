68120edd3c91176507370aabeb0fcc30eff4828a
Marvin <marvinatorrr@gmail.com>
2024-06-13 00:36:47 +0800

    Add project files.

---
+++ Polymorphism/Class1.cs
@@ -0,0 +1,60 @@
+﻿namespace Polymorphism
+{
+    public abstract class DynamicParser
+    {
+        public void Traverse(Node node)
+        {
+            if(node is not null)
+            {
+                DoSomeCustomWork();
+                Traverse(node.Left);
+                Traverse(node.Right);
+            }
+        }
+
+        public abstract void DoSomeCustomWork();
+    }
+
+    public class MyParserA : DynamicParser
+    {
+        public override void DoSomeCustomWork()
+        {
+            var x = 1 + 1;
+        }
+    }
+
+    public class StaticParser<T> where T : IStaticParser, new()
+    {
+        private readonly T _worker = new T();
+
+        public void Traverse(Node node)
+        {
+            if (node is not null)
+            {
+                _worker.DoSomeCustomWork();
+                Traverse(node.Left);
+                Traverse(node.Right);
+            }
+        }
+    }
+
+    public interface IStaticParser
+    {
+        public void DoSomeCustomWork();
+    }
+
+    public class StaticParserA : IStaticParser
+    {
+        public void DoSomeCustomWork()
+        {
+            var x = 1 + 1;
+        }
+    }
+
+    public class Node
+    {
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public int Value { get; set; }
+    }
+}
---
+++ Runner/Program.cs
@@ -0,0 +1,23 @@
+﻿using Polymorphism;
+using System.Diagnostics;
+
+// Dynamic polymorphism test
+var dynamicParser = new MyParserA();
+var dynamicStopWatch = Stopwatch.StartNew();
+for (var i = 0; i < 1_000_000; i++)
+{
+    dynamicParser.Traverse(new Node());
+}
+dynamicStopWatch.Stop();
+Console.WriteLine($"Dynamic polymorphism time elapsed: {dynamicStopWatch.ElapsedMilliseconds} ms");
+
+// Static polymorphism test
+var staticParser = new StaticParser<StaticParserA>();
+var staticStopWatch = Stopwatch.StartNew();
+for (var i = 0; i < 1_000_000; i++)
+{
+    staticParser.Traverse(new Node());
+}
+staticStopWatch.Stop();
+Console.WriteLine($"Static polymorphism time elapsed: {staticStopWatch.ElapsedMilliseconds} ms");
+
